547f7d1e371d37c746d43ef94e63c71890ce7cdf
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-21 19:19:35 -0400

    got rid of NODE_ENV in server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 // ENV Variables
 const PORT = process.env.PORT || 5000;
-const NODE_ENV = process.env.NODE_ENV;
 
 //CORS
 const cors = require('cors');
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 const morgan = require('morgan');
 
 // Middleware
-NODE_ENV === 'production' ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(express.json());
 app.use(morgan('dev')); //logging
 
