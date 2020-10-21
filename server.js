dfd456d28c5df70b9e26eaedd914796493b5b6f5
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-21 19:04:44 -0400

    master-setup: installed dependencies and created server.js
    set up '/' hello world route to test that server working locally and for deployed version

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+// ENV Variables
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
+
+//CORS
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const morgan = require('morgan');
+
+// Middleware
+NODE_ENV === 'production' ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(express.json());
+app.use(morgan('dev')); //logging
+
+// Routes and Routers
+// Route for testing server is working
+app.get('/', (req, res) => {
+	res.json({
+		hello: 'Hello World! Welcome to Tunr',
+	});
+});
+
+//LISTENER
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
